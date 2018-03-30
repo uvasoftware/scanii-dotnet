1f6ba6fe2859302031cf91d178783bd1581efbc7
Rafael Ferreira <raf@ophion.org>
2018-03-30 09:15:28 -0700

    Revving up

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,2 +1,2 @@
-dotnet pack
-dotnet nuget push UvaSoftware.Scanii/bin/Debug/Uvasoftware.Scanii.*.nupkg -k $NUGET_KEY -s https://api.nuget.org/v3/index.json
+dotnet pack --configuration Release
+dotnet nuget push UvaSoftware.Scanii/bin/Release/Uvasoftware.Scanii.*.nupkg -k $NUGET_KEY -s https://api.nuget.org/v3/index.json
