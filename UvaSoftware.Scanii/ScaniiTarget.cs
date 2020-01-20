13c69ad095d2ce38ada8956a9fd2f2646fcb1b88
rafael <raf@uvasoftware.com>
2020-01-19 20:29:22 -0500

    New Regions
    Added 2 new regions: AP2 and EU2
    Fixes #11
    Fixes #9

--- UvaSoftware.Scanii/ScaniiTarget.cs
+++ UvaSoftware.Scanii/ScaniiTarget.cs
@@ -4,11 +4,20 @@
   {
     V20,
     V20Us1,
+    
     V20Eu1,
+    V20Eu2,
+    
+    V20Ap1,
+    V20Ap2,
+    
     V21,
     V21Us1,
+    
     V21Eu1,
-    V20Ap1,
-    V21Ap1
+    V21Eu2,
+    
+    V21Ap1,
+    V21Ap2
   }
 }
