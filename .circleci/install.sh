8e2fc59d5c303fae26cbf1061eda8ae354c37372
rafael <raf@uvasoftware.com>
2020-01-19 20:44:10 -0500

    dumping dotnet core version

--- .circleci/install.sh
+++ .circleci/install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+apt-get -qqy install wget libunwind8 gettext apt-transport-https gpg
+
 # https://docs.microsoft.com/en-us/dotnet/core/linux-prerequisites?tabs=netcore2x
 
-apt-get -qqy install curl libunwind8 gettext apt-transport-https gpg
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-stretch-prod stretch main" > /etc/apt/sources.list.d/dotnetdev.list'
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
+mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+wget -q https://packages.microsoft.com/config/debian/10/prod.list
+mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
 
-apt-get update && apt-get -qqy install dotnet-sdk-2.2
+apt-get update && apt-get -qqy install dotnet-sdk-3.1
 
 echo "dotnet runtime information:"
 dotnet --info
